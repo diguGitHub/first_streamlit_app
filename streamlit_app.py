e593dd93b13915b36f374fe3bbd7f4adc52aa1f4
diguGitHub <45197534+diguGitHub@users.noreply.github.com>
2022-08-11 13:16:03 +0530

    Update streamlit_app.py
    fetching data through snowflake connector and displaying

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,9 @@ fruity_vice_normalized = pandas.json_normalize(fruity_vice_response.json())
 streamlit.dataframe(fruity_vice_normalized)
 
 import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
