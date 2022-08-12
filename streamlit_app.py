93cd8ec2485a70e9e66ddf33755ab7459588128f
diguGitHub <45197534+diguGitHub@users.noreply.github.com>
2022-08-12 12:46:35 +0530

    Update streamlit_app.py
    Modified structure, try catch if else etc

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,20 +21,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#to get user input of fruit choice
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
 #new section to display fruityvice response
-fruity_vice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-
-#normalize the response
-fruity_vice_normalized = pandas.json_normalize(fruity_vice_response.json())
-
-#show as table
-streamlit.dataframe(fruity_vice_normalized)
-
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruity_vice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruity_vice_normalized = pandas.json_normalize(fruity_vice_response.json())
+    streamlit.dataframe(fruity_vice_normalized)
+except URLError as e:
+  streamlit.error()
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
