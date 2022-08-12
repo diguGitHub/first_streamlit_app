df2e7c076f46b5ee9a7145cba42652319c1abda9
diguGitHub <45197534+diguGitHub@users.noreply.github.com>
2022-08-12 12:27:14 +0530

    Update streamlit_app.py
    Did some modifications for insert, moved the imports to the beginning and a put a stop to execution

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -6,7 +11,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -23,7 +27,6 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 #new section to display fruityvice response
-import requests
 fruity_vice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 
 #normalize the response
@@ -32,7 +35,8 @@ fruity_vice_normalized = pandas.json_normalize(fruity_vice_response.json())
 #show as table
 streamlit.dataframe(fruity_vice_normalized)
 
-import snowflake.connector
+streamlit.stop
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -43,3 +47,5 @@ streamlit.dataframe(my_data_rows)
 # option to add a new fruit the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+my_cur.execute("insert into fruit_load_list values("from StreamLit")
